93c1f81cab2c9150b69830a9fe2e5eefee3f2fb9
André Brinkop <andre@brinkop.de>
2016-11-29 10:23:41 +0100

    Initial commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by André Brinkop on 29.11.16.
+//  Copyright © 2016 André Brinkop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
