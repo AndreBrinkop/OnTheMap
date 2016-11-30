f9887b41cf58d258b9cd91a0cd1d97e9f57ea392
André Brinkop <andre@brinkop.de>
2016-11-30 19:35:52 +0100

    Add Facebook SDK and connect "Log in with Facebook" Button

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -7,24 +7,41 @@
 //
 
 import UIKit
+import FacebookLogin
 
 class LoginViewController: UIViewController {
 
     // MARK: Properties
     
     var backgroundGradient: CAGradientLayer?
-    
-    
+
     // MARK: Initialization
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         configureBackground()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         backgroundGradient!.frame = self.view.bounds
+    }
+    
+    // MARK: Facebook Login
+    
+    @IBAction func loginUsingFacebook(_ sender: Any) {
+        let loginManager = LoginManager()
+        loginManager.logIn([ .publicProfile ], viewController: self) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success: //(let grantedPermissions, let declinedPermissions, let accessToken):
+                print("Logged in!")
+            }
+        }
     }
     
     // MARK: Gradient Background
