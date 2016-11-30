ef25905159af05b06b9f391fb8622ea038f5794b
André Brinkop <andre@brinkop.de>
2016-11-30 14:43:26 +0100

    Add styling to login page (including new font)

---
+++ OnTheMap/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  OnTheMap
+//
+//  Created by André Brinkop on 29.11.16.
+//  Copyright © 2016 André Brinkop. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Color {
+    static let lightColor = UIColor(red: 51.0/255.0, green: 100.0/255.0, blue: 168.0/255.0, alpha: 1.0)
+    static let defaultColor = UIColor(red: 9.0/255.0, green:50.0/255.0, blue: 106.0/255.0, alpha: 1.0)
+    static let darkColor = UIColor(red: 3.0/255.0, green: 27.0/255.0, blue: 60.0/255.0, alpha: 1.0)
+}
+
--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -10,14 +10,35 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // MARK: Properties
+    
+    var backgroundGradient: CAGradientLayer?
+    
+    
+    // MARK: Initialization
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        configureBackground()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        backgroundGradient!.frame = self.view.bounds
+    }
+    
+    // MARK: Gradient Background
+    
+    func configureBackground() {
+        view.backgroundColor = Color.defaultColor
+        backgroundGradient = CAGradientLayer()
+        let colorTopBottom = Color.defaultColor.cgColor
+        let colorMiddle = Color.lightColor.cgColor
+        
+        backgroundGradient!.colors = [colorTopBottom, colorMiddle, colorMiddle, colorTopBottom]
+        backgroundGradient!.locations = [0.0, 0.3, 0.7, 1.0]
+        backgroundGradient!.frame = view.bounds
+        view.layer.insertSublayer(backgroundGradient!, at: 0)
     }
 
 
